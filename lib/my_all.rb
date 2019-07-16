86c5661a5d1506bf9dbf7fba476fb6ce308a5c9b
Laura Radville <laura.radville@gmail.com>
2019-07-16 13:50:05 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -10,9 +10,9 @@ def my_all?(collection)
   block_return_values
 end
 
-# if block_return_values.include?(false)
-#     false
-#   else
-#     true
-#   end
-# end
+if block_return_values.include?(false)
+    false
+  else
+    true
+  end
+end
