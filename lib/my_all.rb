098f06749a6eead9e5ec4a1ddf700c57a84ed93f
Laura Radville <laura.radville@gmail.com>
2019-07-16 13:50:30 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,12 +7,10 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1
   end
-  block_return_values
-end
-
-if block_return_values.include?(false)
+  if block_return_values.include?(false)
     false
   else
     true
   end
 end
+
